ad22a098fad0a637763b30dd05d2dbe2cd463f1a
Kyu Lee <kwooler@gmail.com>
2023-06-09 23:57:50 -0700

    M4Q1

--- main_test.py
+++ main_test.py
@@ -7,32 +7,11 @@ import types
 
 
 def test_main_1():
-    # captureOut = io.StringIO()
-    # sys.stdout = captureOut
-    # datastr = '35 5 10 20 40 15'
-    # sys.stdin = io.StringIO(datastr)
-
-    # main.main()
-    # sys.stdout = sys.__stdout__
-    # print('Captured ', captureOut.getvalue())
-    # lines = captureOut.getvalue().split('\n')
-    # print(lines)
 
     main.main()
     students = main.makeList()
-    assert len(students) == 5
     main.printList(students)
-
-    ret = main.scorebySubject(students)
-
-    # regex_string = r'[\w,\W]*1'
-    # regex_string += r'[\w,\W]*3'
-    # regex_string += r'[\w,\W]*5'
-    # regex_string += r'[\w,\W]*'
-    # print(regex_string)
-    # res = re.search(regex_string, main.evenlist)
-    # assert res != None
-    # print(res.group())
+    assert len(students) == 5
 
 
 def test_main_2():
@@ -49,6 +28,9 @@ def test_main_3():
     students = main.makeList()
     ret = main.findStudents(students)
     assert len(ret) == 3
+    assert ret[0]['id'] == 1003
+    assert ret[1]['id'] == 1004
+    assert ret[2]['id'] == 1005
 
 
 def test_main_4():
@@ -56,3 +38,5 @@ def test_main_4():
     ret = main.getAvgList(students)
     assert len(ret) == 5
     assert ret[0] == 77.5
+    assert ret[1] == 82.5
+    assert ret[4] == 87.5
